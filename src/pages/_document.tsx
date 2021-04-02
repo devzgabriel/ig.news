0366843093755a55492852a131a31269c9dde3f2
Gabriel Silva <devzgabriel@gmail.com>
2021-04-02 18:53:34 -0300

    add styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,6 @@ export default class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,900&display=swap'
             rel='stylesheet'
           />
-          <title>ig.news</title>
         </Head>
         <body>
           <Main />
