afb64f99d8e7d885d547dad1185a0803910ad232
Gabriel Silva <devzgabriel@gmail.com>
2021-04-12 22:02:49 -0300

    get Prismic data and show list

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,23 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
+
 import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
 
-export default function Posts() {
+interface Post {
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
+
+interface PostProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -13,30 +26,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href='#'>
-            <time>12 de Março de 3389</time>
-            <strong>Titulo lindão de um post</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A vel
-              debitis provident nemo, facilis perspiciatis dolorem quisqua
-            </p>
-          </a>
-          <a href='#'>
-            <time>12 de Março de 3389</time>
-            <strong>Titulo lindão de um post</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A vel
-              debitis provident nemo, facilis perspiciatis dolorem quisqua
-            </p>
-          </a>
-          <a href='#'>
-            <time>12 de Março de 3389</time>
-            <strong>Titulo lindão de um post</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A vel
-              debitis provident nemo, facilis perspiciatis dolorem quisqua
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href='#'>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -46,7 +42,7 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient()
 
-  const response = prismic.query(
+  const response = await prismic.query(
     [Prismic.predicates.at('document.type', 'post')],
     {
       fetch: ['publication.title', 'publication.content'],
@@ -54,7 +50,27 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   )
 
+  console.log(JSON.stringify(response, null, 2))
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'paragraph')
+          ?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    }
+  })
+
   return {
-    props: {},
+    props: { posts },
   }
 }
