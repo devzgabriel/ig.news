5a4e8040f9276ef9d17fd90c6138ebb3e1bbeed9
Gabriel Silva <devzgabriel@gmail.com>
2021-04-17 20:56:28 -0300

    validando assinatura

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,10 +41,16 @@ export const getServerSideProps: GetServerSideProps = async ({
   params,
 }) => {
   const session = await getSession({ req })
-
   const { slug } = params
 
-  console.log(session)
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
 
   const prismic = getPrismicClient(req)
   const response = await prismic.getByUID('post', String(slug), {})
